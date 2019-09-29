95dd1bc74ab24fb8ccf7efbe8a88a247bd9ac6e0
sergeyampo <dev@sergeyampo.ru>
2019-09-30 05:34:18 +0900

    Added Column Names functions

--- OOP_MIET/MailInteract.h
+++ OOP_MIET/MailInteract.h
@@ -13,35 +13,35 @@ public:
 	//Общая функция ввода всех полей
 	static Mail InputData() {
 		Mail Buffer;
-		string input;
-		cout << "Enter index: ";
+		std::string input;
+		std::cout << "Enter index: ";
 		Buffer.index = CorrectInput::EnterIntNum();
-		ClearCin(cin);
+		ClearCin(std::cin);
 
-		cout << "Enter reciever adresss: ";
-		getline(cin, Buffer.reciever_adress);
+		std::cout << "Enter reciever adresss: ";
+		getline(std::cin, Buffer.reciever_adress);
 
-		cout << "Enter reciever name: ";
+		std::cout << "Enter reciever name: ";
 		Buffer.reciever_name = CorrectInput::EnterSym();
-		ClearCin(cin);
+		ClearCin(std::cin);
 
-		cout << "Enter sender adresss: ";
-		getline(cin, Buffer.sender_adress);
+		std::cout << "Enter sender adresss: ";
+		getline(std::cin, Buffer.sender_adress);
 
-		cout << "Enter sender name: ";
+		std::cout << "Enter sender name: ";
 		Buffer.sender_name = CorrectInput::EnterSym();
-		ClearCin(cin);
+		ClearCin(std::cin);
 
-		cout << "Enter cost: ";
+		std::cout << "Enter cost: ";
 		Buffer.cost = CorrectInput::EnterDoubleNum();
-		ClearCin(cin);
+		ClearCin(std::cin);
 		
 		return Buffer;
 	}
 
 	static auto GetFindCritery() {
-		cout << "Type the sender adress to find: ";
-		string buffer;
+		std::cout << "Type the sender adress to find: ";
+		std::string buffer;
 		ClearCin(std::cin);
 		getline(std::cin, buffer);
 
@@ -49,22 +49,26 @@ public:
 	}
 
 	static auto GetFilterCritery() {
-		cout << "Type the index for filtering: ";
+		std::cout << "Type the index for filtering: ";
 		unsigned int buffer = CorrectInput::EnterIntNum();
 		return [buffer](Mail m) { return m.index == buffer; };
 	}
 
 	static auto GetSortCritery() {
-		cout << "Sorting by increasing cost.\n";
+		std::cout << "Sorting by increasing cost.\n";
 		return [](Mail x, Mail y) { return x.cost < y.cost; };
 	}
 
 	static void OutputData(Mail buffer) {
-		cout << buffer.index << "\t" << buffer.reciever_adress << "\t" << buffer.reciever_name << "\t" << buffer.sender_adress << "\t" << buffer.sender_name << "\t" << buffer.cost << "\n";
+		std::cout << buffer.index << "\t" << buffer.reciever_adress << "\t" << buffer.reciever_name << "\t" << buffer.sender_adress << "\t" << buffer.sender_name << "\t" << buffer.cost << "\n";
 	}
 
-	static const string& GetFilename() {
+	static const std::string& GetFilename() {
 		return filename;
+	}
+
+	static void PrintColumnNames() {
+		std::cout << "Index\t" << "Rec. Adress\t" << "Rec. Name\t" << "Sen. Adress\t" << "Sen. Name\t" << "Cost\n";
 	}
 
   private:
