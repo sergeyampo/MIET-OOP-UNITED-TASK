e107778247378501f5f24d11b6a5189fe88d306b
Владислав <@KATSURA-PC>
2019-10-14 02:38:44 +0300

    Add NewPrintTable() -- IN PROCESS

--- OOP_MIET/InteractDB.h
+++ OOP_MIET/InteractDB.h
@@ -2,6 +2,8 @@
 #include "Database.h"
 #include "CorrectInput.h"
 #include "MailInteract.h"
+#include "StudentInteract.h"
+//#include "FoodInteract.h"
 #include <iostream>
 
 //Универсальный интерфейс работы с базой данных
@@ -144,7 +146,7 @@ namespace InteractDB {
 			return SaveDb<InteractType>(db);
 		}
 	}
-
+	
 	//Функция получает базу данных и выводит её в виде таблицы
 	template <class InteractType, class ItemType>
 	void PrintTable(Database<ItemType>& db) {
@@ -157,8 +159,46 @@ namespace InteractDB {
 			InteractType::OutputData(db[i]);
 	}
 
+	//Функция получает базу данных и выводит её в виде КРАСИВОЙ таблицы -- IN PROCESS
+	template <class InteractType, class ItemType>
+	void NewPrintTable(Database<ItemType>& db) {
+
+		//PUT IT AWAY (later)
+		PrintTable<InteractType>(db);
+
+		if (db.Empty()) {
+			std::cout << "There's nothing to show!\n";
+			return;
+		}
+
+		string Header = InteractType::GetTableHeader();
+		
+		//Обработаем TableHeader, получим кол-во полей и их длины
+		int i = 0;
+		int NumOfFields = 1;
+		while (Header[i] != '\n') {
+			if (Header[i] == ' ') ++NumOfFields;
+			++i;
+		}
+		cout << "Number of fields = " << NumOfFields << '\n';
+		return;
+
+		/*
+		int ibeg = 0;
+		int i = 0;
+		while (TableHeader[i] != '\n') {
+			while (TableHeader[i] != '\n' && TableHeader[i] != ' ') {
+				++i;
+			}
+			if (TableHeader[i] == '\n') break;
+			//Length = a;
+			++i;
+		}
+		*/
+
+		//for (int i = 0; i < TableHeader.Size
+		InteractType::PrintColumnNames();
+		for (int i = 0; i < db.Size(); ++i)
+			InteractType::OutputData(db[i]);
+	}
 };
-
-
-
-
