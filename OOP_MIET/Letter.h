45c4ae09f63152533b7e41418e2b58668aede33b
sergeyampo <dev@sergeyampo.ru>
2019-11-04 23:48:29 +0300

    Added two derived classes from Letter base class.

--- OOP_MIET/Letter.h
+++ OOP_MIET/Letter.h
@@ -5,31 +5,31 @@
 
 class Letter {
   public:
-	Letter(const unsigned int& ind, const Man& rec, const Man& sen, const float& cst) : index(ind), reciever(rec), sender(sen), cost(cst) { }
+	Letter(const unsigned int& ind, const Man& rec, const Man& sen) : index(ind), reciever(rec), sender(sen) { }
 	Letter() = default;
-   ~Letter() = default;
+    virtual ~Letter() = default;
     Letter(const Letter&) = default;
     Letter(Letter&&) = default;
 
 
-	bool Empty() const { return index == 0 && reciever.Empty() && sender.Empty() && cost == 0.00; }
+	virtual bool Empty() const { return index == 0 && reciever.Empty() && sender.Empty(); }
 
 	//Сериализация
 	template<class Archive>
 	void serialize(Archive & archive) {
-		archive(index, reciever, sender, cost);
+		archive(index, reciever, sender);
 	}
 
-	Letter& operator=(const Letter& rhs) { std::tie(index, reciever, sender, cost) = std::tie(rhs.index, rhs.reciever, rhs.sender, rhs.cost);
+	Letter& operator=(const Letter& rhs) { std::tie(index, reciever, sender) = std::tie(rhs.index, rhs.reciever, rhs.sender);
 	                                   return *this; }
 	
-	std::ostream& Write(std::ostream& out) {
+	virtual std::ostream& Write(std::ostream& out) {
 		out << index << "\t";
 		reciever.Write(out);
 		out << "\t";
 		sender.Write(out);
 		out << "\t";
-		out << cost << "\n";
+
 		return out;
 	}
 
@@ -48,18 +48,69 @@ class Letter {
 	std::string GetSenderName() const noexcept { return sender.GetName(); }
 	void SetSenderName(const std::string& arg) { sender.SetName(arg); }
 
-	double GetCost() const noexcept { return cost; }
-	void SetCost(const double& arg) { cost = arg; }
-
-	//SENSELESS, Erase it!
-	bool operator==(const Man& rhs) { return sender.GetAddress() == rhs.GetAddress(); }
-
-
   private:
    unsigned int index;
    Man reciever;
    Man sender;
-   double cost;
+};
+
+//Письмо, имеющее массу
+class Letter1 : public Letter {
+	Letter1() : Letter() {}
+   ~Letter1() = default;
+    Letter1(const Letter1& l) : Letter(l) { }
+	Letter1(const unsigned int& ind, const Man& rec, const Man& sen, const double& wght) : Letter(ind, rec, sen), weight(wght) { }
+	
+	bool Empty() const override { return Letter::Empty() && weight == 0.00; }
+
+	//Сериализация
+	template<class Archive>
+	void serialize(Archive & archive) {
+		archive(cereal::base_class<Letter>(this), weight);
+	}
+
+	std::ostream& Write override(std::ostream& out) {
+		out = Letter::Write(out);
+		out << weight << "\n";
+
+		return out;
+	}
+
+	double GetWeight() const noexcept { return weight; }
+	void SetWeight(const double& arg) { weight = arg; }
+
+  private:
+	  double weight;
+};
+
+//Письмо имеющее стоимость
+class Letter2 : public Letter {
+  Letter2() : Letter() {}
+ ~Letter2() = default;
+  Letter2(const Letter2& l) : Letter(l) { }
+  Letter2(const unsigned int& ind, const Man& rec, const Man& sen, const double& cst) : Letter(ind, rec, sen, cst), cost(cst) { }
+
+  bool Empty() const override { return Letter::Empty() && cost == 0.00; }
+
+  std::ostream& Write override(std::ostream& out) {
+	  out = Letter::Write(out);
+	  out << weight << "\n";
+
+	  return out;
+  }
+
+  std::ostream& Write override(std::ostream& out) {
+	  out = Letter::Write(out);
+	  out << cost << "\n";
+
+	  return out;
+  }
+
+  double GetCost() const noexcept { return cost; }
+  void SetCost(const double& arg) { cost = arg; }
+
+  private:
+	  double cost;
 };
 
 inline std::ostream& operator<<(std::ostream& out, Letter& m) {
