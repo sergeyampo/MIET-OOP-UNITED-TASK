d9b409278ab6039b1b86135210dbd68acbffd085
sergeyampo <dev@sergeyampo.ru>
2019-11-10 00:26:21 +0300

    Correct some deception about polymorphism, make real inheritance but with some inevitable useless virtual methods in the base class

--- OOP_MIET/Letter.h
+++ OOP_MIET/Letter.h
@@ -5,7 +5,7 @@
 
 class Letter {
   public:
-	Letter(const unsigned int& ind, const Man& rec, const Man& sen, const double& cst) : index(ind), reciever(rec), sender(sen), cost(cst) { }
+	Letter(const unsigned int& ind, const Man& rec, const Man& sen) : index(ind), reciever(rec), sender(sen) { }
 	Letter() = default;
     virtual ~Letter() = default;
     Letter(const Letter&) = default;
@@ -37,7 +37,7 @@ class Letter {
 	void SetIndex(const unsigned& arg) { index = arg; }
 
 	std::string GetRecieverAdress() const noexcept { return reciever.GetAddress(); }
-	void SetRecieverAddress(const std::string& arg) { reciever.SetName(arg); }
+	void SetRecieverAddress(const std::string& arg) { reciever.SetAddress(arg); }
 	
 	std::string GetRecieverName() const noexcept { return reciever.GetName(); }
 	void SetRecieverName(const std::string& arg) { reciever.SetName(arg); }
@@ -48,22 +48,23 @@ class Letter {
 	std::string GetSenderName() const noexcept { return sender.GetName(); }
 	void SetSenderName(const std::string& arg) { sender.SetName(arg); }
 
-	virtual double GetCost() const noexcept { return cost; }
-	virtual void SetCost(const double& arg) { cost = arg; }
+	virtual double GetCost() const noexcept { return 0; }
+	virtual double GetWeight() const noexcept { return 0; }
 
   private:
    unsigned int index;
    Man reciever;
    Man sender;
-   double cost;
 };
 
 //Письмо, имеющее массу
 class Letter1 : public Letter {
+  public:
 	Letter1() : Letter() {}
    ~Letter1() = default;
     Letter1(const Letter1& l) : Letter(l) { }
-	Letter1(const unsigned int& ind, const Man& rec, const Man& sen, const double& wght, const double& cst) : Letter(ind, rec, sen, cst), weight(wght) { }
+	Letter1(const unsigned int& ind, const Man& rec, const Man& sen, const double& wght, const double& cst) : Letter(ind, rec, sen), weight(wght) { }
+	Letter1(const Letter& l, const double& w) : Letter(l), weight(w) { }
 	
 	bool Empty() const override { return Letter::Empty() && weight == 0.00; }
 
@@ -80,8 +81,9 @@ class Letter1 : public Letter {
 		return out;
 	}
 
-	double GetWeight() const noexcept { return weight; }
+	double GetWeight() const noexcept override { return weight; }
 	void SetWeight(const double& arg) { weight = arg; }
+	double GetCost() const noexcept override { return 0; }
 
   private:
 	  double weight;
@@ -89,22 +91,31 @@ class Letter1 : public Letter {
 
 //Письмо имеющее стоимость
 class Letter2 : public Letter {
-  Letter2() : Letter() {}
- ~Letter2() = default;
-  Letter2(const Letter2& l) : Letter(l) { }
-  Letter2(const unsigned int& ind, const Man& rec, const Man& sen, const double& cst) : Letter(ind, rec, sen, cst), cost(cst) { }
+ public:
+   Letter2() : Letter() {}
+  ~Letter2() = default;
+   Letter2(const Letter2& l) : Letter(l) { }
+   Letter2(const unsigned int& ind, const Man& rec, const Man& sen, const double& cst) : Letter(ind, rec, sen), cost(cst) { }
+   Letter2(const Letter& l, const double& cst) : Letter(l) { SetCost(cst); }
 
-  bool Empty() const override { return Letter::Empty() && cost == 0.00; }
+   bool Empty() const override { return Letter::Empty() && cost == 0.00; }
 
-  std::ostream& Write(std::ostream& out) override {
-	  Letter::Write(out);
-	  out << cost << "\n";
+   std::ostream& Write(std::ostream& out) override {
+	   Letter::Write(out);
+	   out << cost << "\n";
 
-	  return out;
-  }
+	   return out;
+   }
+
+   //Сериализация
+   template<class Archive>
+   void serialize(Archive & archive) {
+	   archive(cereal::base_class<Letter>(this), cost);
+   }
 
   double GetCost() const noexcept override { return cost; }
-  void SetCost(const double& arg) override { cost = arg; }
+  void SetCost(const double& arg) { cost = arg; }
+  double GetWeight() const noexcept override { return 0; }
 
   private:
 	  double cost;
